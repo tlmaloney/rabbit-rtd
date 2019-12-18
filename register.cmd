7a5cf7d526fe432c051e278ca5d8b6f89effb5da
Thomas Maloney <thomas.l.maloney@gmail.com>
2019-12-18 18:44:35 -0500

    Update register.cmd
    Updates register.cmd for 64bit Excel

--- register.cmd
+++ register.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 echo Registering RTD Server
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe RabbitRtd\bin\Release\RabbitRTD.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe RabbitRtd\bin\Release\RabbitRTD.dll /codebase
 
 if errorlevel 1 pause
